b92de0a772b3cca66939b900843aa8e80235358a
Keneth Miranda <kennethmiranda56@gmail.com>
2021-11-05 17:45:58 -0600

    tocando el CSS

--- client/blog/src/app/app.component.ts
+++ client/blog/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { TokenStorageService } from './services/token-storage.service';
   selector: 'app-root',//va a ser la forma como nuestro componente va a ser llamado dentro del modulo,
   //en este caso el app-root es requerido en la pagina HTML
   templateUrl: './app.component.html',//cual va a ser el url de nuestro html
-  styleUrls: ['./app.component.scss']// en este defiene cual va a ser la hoja de estilos del componente
+  styleUrls: ['./app.css']// en este defiene cual va a ser la hoja de estilos del componente
 })
 export class AppComponent {// en esta clase ira la logica
   title = 'TicoAirlines';
